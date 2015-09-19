42a1eed9c5615001ce4357254d1154e49eab870a
robinsinghdev <robin.singh@stavyah.com>
2015-09-19 19:08:11 +0530

    www folder

---
+++ www/js/index.js
@@ -0,0 +1,210 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var app = {
+    SOME_CONSTANTS : false,  // some constant
+
+
+    // Application Constructor
+    initialize: function() {
+        console.log("console log init");
+        this.bindEvents();
+        this.initFastClick();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    initFastClick : function() {
+        window.addEventListener('load', function() {
+            FastClick.attach(document.body);
+        }, false);
+    },
+    // Phonegap is now ready...
+    onDeviceReady: function() {
+        console.log("device ready, start making you custom calls!");
+
+        // Start adding your code here....
+		//app.receivedEvent('deviceready');
+		alert('fdgdf');
+		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+		db.transaction(initializeDB, errorCB, successCB);
+		alert('successCB');
+    },
+	// Update DOM on a Received Event
+    receivedEvent: function(id) {
+		
+    }
+};
+
+function getTodayDate(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1;//January is 0, so always add + 1
+
+	var yyyy = today.getFullYear();
+	if(dd<10){dd='0'+dd}
+	if(mm<10){mm='0'+mm}
+	today = dd+'/'+mm+'/'+yyyy;
+	return today;
+}
+
+function startTracker() {
+   //$('#timeTrackerDiv').timer();
+   $('#timeTrackerDiv').timer({
+		seconds: 200 //Specify start time in seconds
+	});
+	
+	var startTimerDiv = document.getElementById("startTimerDiv");
+	startTimerDiv.setAttribute('style', 'display:none;');
+	var endTimerDiv = document.getElementById("endTimerDiv");
+	endTimerDiv.setAttribute('style', 'display:block;');
+	
+	var currtimetrackerid; 
+	var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+	db.transaction(function(tx) {
+		tx.executeSql('INSERT INTO DEMO(data, tracker_date) VALUES (?,?)',["0",getTodayDate().toString()]
+			,function(tx, results){
+					//alert('Returned ID: ' + results.insertId);
+					currTimeTrackerId=results.insertId;
+					window.localStorage.setItem("trackerkey", ""+currTimeTrackerId+"");
+			 }
+		);
+	});
+} 
+
+function stopTracker() {
+	//var trackerkey=window.localStorage.getItem("trackerkey");
+	var timeTracked=$('#timeTrackerDiv').data('seconds'); 
+	var currtimetrackerid = window.localStorage.getItem("trackerkey");
+	
+	var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+	db.transaction(function(tx) {
+		//tx.executeSql('INSERT INTO DEMO(data, tracker_date) VALUES (?,?)',[timeTracked.toString(),getTodayDate().toString()]);
+		tx.executeSql("UPDATE DEMO SET data='" + timeTracked + "' WHERE id=' "+currtimetrackerid+" '");
+		
+		/*
+		var executeQuery = "UPDATE " +"DEMO " +"SET data = ?  WHERE  id =?"; 
+		transaction.executeSql(executeQuery, [timeTracked, currtimetrackerid]
+			,
+			function(tx, result) {   // On success
+				 alert('updated successfully.');
+			}
+		);
+		*/
+	});
+	window.localStorage.removeItem("trackerkey");
+	$('#timeTrackerDiv').timer('remove');
+	
+	var startTimerDiv = document.getElementById("startTimerDiv");
+	startTimerDiv.setAttribute('style', 'display:block;');
+	var endTimerDiv = document.getElementById("endTimerDiv");
+	endTimerDiv.setAttribute('style', 'display:none;');
+}
+			
+function AddValueToDB() {
+   //alert('Databases are nowwww supported in this browser.');
+   var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+   db.transaction(initializeDB, errorCB, successCB);
+} 
+
+function GetValueToDB() {
+   //alert('Databases are nowwww supported in this browser.');
+   var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+   db.transaction(successCBData, errorCB);
+} 
+
+// Populate the database 
+function initializeDB(tx) {
+	//tx.executeSql('DROP TABLE IF EXISTS DEMO');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id integer primary key autoincrement, data text,tracker_date text)');
+	//tx.executeSql('INSERT INTO DEMO (id, data, tracker_date) VALUES (1, "15Sep2015 row", "15Sep2015")');
+	tx.executeSql('INSERT INTO DEMO(data, tracker_date) VALUES (?,?)',["15Sep2015 row","15/09/2015"]);
+	tx.executeSql('INSERT INTO DEMO(data, tracker_date) VALUES (?,?)',["16Sep2015","16/09/2015"]);
+}
+
+// Insert Values in the database 
+function insertValueInDB(tx) {
+	//tx.executeSql('DROP TABLE IF EXISTS DEMO');
+	//tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id integer primary key unique, data text,tracker_date text)');
+	//tx.executeSql('INSERT INTO DEMO (id, data, tracker_date) VALUES (1, "15Sep2015 row", "15Sep2015")');
+}
+
+// Query the database
+function queryDB(tx) {
+	tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+function queryDBData(tx) {
+	tx.executeSql('SELECT * FROM DEMO', [], querySuccessData, errorCB);
+}
+
+// Query the success callback
+function querySuccess(tx, results) {
+	console.log("Returned rows = " + results.rows.length);
+	alert("Returned rows = " + results.rows.length);
+	// this will be true since it was a select statement and so rowsAffected was 0
+	if (!results.rowsAffected) {
+		console.log('No rows affected!');
+		return false;
+	}
+	// for an insert statement, this property will return the ID of the last inserted row
+	console.log("Last inserted row ID = " + results.insertId);
+}
+
+function querySuccessData(tx, results) {
+	console.log("Returned rows = " + results.rows.length);
+	
+		var len = results.rows.length;
+		console.log("DEMO table: " + len + " rows found.");
+		$("#trackerList > li").remove();
+		for (var i=0; i<len; i++){
+			//alert("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
+			//console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
+			$('#trackerList').append('<li><a href="#">' + results.rows.item(i).data + '--' +results.rows.item(i).tracker_date+'</a></li>').listview('refresh');
+		}
+	
+	//alert("Returned rows = " + results.rows.length);
+	// this will be true since it was a select statement and so rowsAffected was 0
+	if (!results.rowsAffected) {
+		console.log('No rows affected!');
+		return false;
+	}
+	// for an insert statement, this property will return the ID of the last inserted row
+	console.log("Last inserted row ID = " + results.insertId);
+}
+
+// Transaction error callback
+function errorCB(err) {
+	console.log("Error processing SQL: "+err.code);
+}
+
+// Transaction success callback
+function successCB() {
+	var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+	db.transaction(queryDB, errorCB);
+}
+
+function successCBData() {
+	var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+	db.transaction(queryDBData, errorCB);
+}
